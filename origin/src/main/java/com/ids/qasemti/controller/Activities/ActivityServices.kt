8f112326e63a317ee1633afa866076955a4c1c9e
ibrahim <i.haydar@ids.com.lb>
2021-09-01 14:21:38 +0300

    latest

---
+++ origin/src/main/java/com/ids/qasemti/controller/Activities/ActivityServices.kt
@@ -0,0 +1,58 @@
+package com.ids.qasemti.controller.Activities
+
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ids.qasemti.R
+import com.ids.qasemti.controller.Adapters.AdapterMyServices
+import com.ids.qasemti.controller.Adapters.AdapterServices
+import com.ids.qasemti.controller.Adapters.RVOnItemClickListener.RVOnItemClickListener
+import com.ids.qasemti.controller.Base.ActivityBase
+import com.ids.qasemti.model.ServiceItem
+import com.ids.qasemti.utils.AppHelper
+import com.ids.qasemti.utils.hide
+import com.ids.qasemti.utils.show
+import kotlinx.android.synthetic.main.activity_services.*
+import kotlinx.android.synthetic.main.no_logo_layout.*
+
+
+class ActivityServices : ActivityBase(),RVOnItemClickListener {
+    var array : ArrayList<ServiceItem> = arrayListOf()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_services)
+        AppHelper.setAllTexts(rootLayout)
+        init()
+        listeners()
+        setData()
+
+    }
+
+    private fun init(){
+        btBck.show()
+    }
+
+    private fun listeners(){
+        btBck.setOnClickListener{super.onBackPressed()}
+    }
+
+    private fun setData(){
+
+        array.add(ServiceItem("Water Tank","", R.drawable.icon_water))
+        array.add(ServiceItem("Sand Truck","", R.drawable.icon_truck))
+        array.add(ServiceItem("Rubble Truck for Long Term","", R.drawable.icon_rubble_truck))
+
+
+        var adapter = AdapterMyServices(array,this,this)
+        rvServices.layoutManager = LinearLayoutManager(this)
+        rvServices.adapter = adapter
+        rvServices.isNestedScrollingEnabled = false
+    }
+
+    override fun onItemClicked(view: View, position: Int) {
+
+    }
+}
--- origin/src/main/java/com/ids/qasemti/controller/Fragments/FragmentOrders.kt
+++ origin/src/main/java/com/ids/qasemti/controller/Fragments/FragmentOrders.kt
@@ -2,9 +2,12 @@ package com.ids.qasemti.controller.Fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.ids.qasemti.R
@@ -19,7 +22,7 @@ import kotlinx.android.synthetic.main.fragment_orders.*
 
 class FragmentOrders : Fragment() , RVOnItemClickListener {
 
-
+    var ordersArray : ArrayList<String> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -35,136 +38,30 @@ class FragmentOrders : Fragment() , RVOnItemClickListener {
         super.onViewCreated(view, savedInstanceState)
         AppHelper.setAllTexts(rootLayoutOrderType)
         init()
+        setTabs()
+        setTabLayout(0,tvActive)
+        setData(true)
 
     }
 
     fun init(){
-
-        if(typeSelected==1){
-            tvActive.setBackgroundResource(R.drawable.rounded_red_background)
-            tvCompleted.setBackgroundResource(R.color.transparent)
-            tvUpcoming.setBackgroundResource(R.color.transparent)
-            tvCancelled.setBackgroundResource(R.color.transparent)
-
-            AppHelper.setTextColor(requireContext(),tvActive,R.color.white)
-            AppHelper.setTextColor(requireContext(),tvUpcoming,R.color.redPrimary)
-            AppHelper.setTextColor(requireContext(),tvCancelled,R.color.redPrimary)
-            AppHelper.setTextColor(requireContext(),tvCompleted,R.color.redPrimary)
-
-        }else{
-            tvUpcoming.setBackgroundResource(R.drawable.rounded_red_background)
-            tvCompleted.setBackgroundResource(R.color.transparent)
-            tvActive.setBackgroundResource(R.color.transparent)
-            tvCancelled.setBackgroundResource(R.color.transparent)
-
-
-            AppHelper.setTextColor(requireContext(),tvActive,R.color.redPrimary)
-            AppHelper.setTextColor(requireContext(),tvUpcoming,R.color.white)
-            AppHelper.setTextColor(requireContext(),tvCancelled,R.color.redPrimary)
-            AppHelper.setTextColor(requireContext(),tvCompleted,R.color.redPrimary)
-        }
-        var ordersArray : ArrayList<String> = arrayListOf()
-
         ordersArray.clear()
         ordersArray.add("1")
         ordersArray.add("1")
         ordersArray.add("1")
 
-        var adapter = AdapterOrderType(ordersArray, this, requireContext(),true)
-        rvOrderDetails.adapter = adapter
-        var glm2 = GridLayoutManager(requireContext(), 1)
-        rvOrderDetails.layoutManager = glm2
-
         (activity as ActivityHome?)!!.drawColor()
 
-        tvActive.setOnClickListener {
-            if(typeSelected!=1){
-                typeSelected=1
-                tvActive.setBackgroundResource(R.drawable.rounded_red_background)
-                tvCompleted.setBackgroundResource(R.color.transparent)
-                tvUpcoming.setBackgroundResource(R.color.transparent)
-                tvCancelled.setBackgroundResource(R.color.transparent)
+    }
 
-                AppHelper.setTextColor(requireContext(),tvActive,R.color.white)
-                AppHelper.setTextColor(requireContext(),tvUpcoming,R.color.redPrimary)
-                AppHelper.setTextColor(requireContext(),tvCancelled,R.color.redPrimary)
-                AppHelper.setTextColor(requireContext(),tvCompleted,R.color.redPrimary)
-
-                var adapter = AdapterOrderType(ordersArray, this, requireContext(),true)
-                rvOrderDetails.adapter = adapter
-                var glm2 = GridLayoutManager(requireContext(), 1)
-                rvOrderDetails.layoutManager = glm2
-
-            }
-
-
-        }
-        tvUpcoming.setOnClickListener {
-            if(typeSelected!=2){
-                typeSelected=2
-                tvUpcoming.setBackgroundResource(R.drawable.rounded_red_background)
-                tvCompleted.setBackgroundResource(R.color.transparent)
-                tvActive.setBackgroundResource(R.color.transparent)
-                tvCancelled.setBackgroundResource(R.color.transparent)
-
-
-                AppHelper.setTextColor(requireContext(),tvActive,R.color.redPrimary)
-                AppHelper.setTextColor(requireContext(),tvUpcoming,R.color.white)
-                AppHelper.setTextColor(requireContext(),tvCancelled,R.color.redPrimary)
-                AppHelper.setTextColor(requireContext(),tvCompleted,R.color.redPrimary)
-
-                var adapter = AdapterOrderType(ordersArray, this, requireContext(),true)
-                rvOrderDetails.adapter = adapter
-                var glm2 = GridLayoutManager(requireContext(), 1)
-                rvOrderDetails.layoutManager = glm2
-            }
-
-
-        }
-        tvCancelled.setOnClickListener {
-            if(typeSelected!=3){
-                typeSelected=3
-                tvCancelled.setBackgroundResource(R.drawable.rounded_red_background)
-                tvCompleted.setBackgroundResource(R.color.transparent)
-                tvActive.setBackgroundResource(R.color.transparent)
-                tvUpcoming.setBackgroundResource(R.color.transparent)
-
-
-                AppHelper.setTextColor(requireContext(),tvActive,R.color.redPrimary)
-                AppHelper.setTextColor(requireContext(),tvUpcoming,R.color.redPrimary)
-                AppHelper.setTextColor(requireContext(),tvCancelled,R.color.white)
-                AppHelper.setTextColor(requireContext(),tvCompleted,R.color.redPrimary)
-
-                var adapter = AdapterOrderCancelled(ordersArray, this, requireContext(),false)
-                rvOrderDetails.adapter = adapter
-                var glm2 = GridLayoutManager(requireContext(), 1)
-                rvOrderDetails.layoutManager = glm2
-
-            }
-
-
-
-        }
-        tvCompleted.setOnClickListener {
-            if(typeSelected!=4){
-                typeSelected=4
-                tvCompleted.setBackgroundResource(R.drawable.rounded_red_background)
-                tvCancelled.setBackgroundResource(R.color.transparent)
-                tvActive.setBackgroundResource(R.color.transparent)
-                tvUpcoming.setBackgroundResource(R.color.transparent)
-
-
-                AppHelper.setTextColor(requireContext(),tvActive,R.color.redPrimary)
-                AppHelper.setTextColor(requireContext(),tvUpcoming,R.color.redPrimary)
-                AppHelper.setTextColor(requireContext(),tvCancelled,R.color.redPrimary)
-                AppHelper.setTextColor(requireContext(),tvCompleted,R.color.white)
-
-
-                var adapter = AdapterOrderType(ordersArray, this, requireContext(),false)
-                rvOrderDetails.adapter = adapter
-                var glm2 = GridLayoutManager(requireContext(), 1)
-                rvOrderDetails.layoutManager = glm2
-
+    private fun setTabs(){
+        for (i in 0 until linearTabs.childCount){
+            linearTabs.getChildAt(i).setOnClickListener{
+                if(typeSelected!=i){
+                    var tv=linearTabs.getChildAt(i) as TextView
+                    setTabLayout(i,tv)
+                    setData()
+                }
             }
         }
     }
@@ -173,5 +70,26 @@ class FragmentOrders : Fragment() , RVOnItemClickListener {
         if(view.id==R.id.llLocation){
             startActivity(Intent(requireActivity(), ActivityMap::class.java))
         }
+    }
+
+    fun setTabLayout(position: Int,tvSelected:TextView){
+        typeSelected=position
+        for (i in 0 until linearTabs.childCount){
+            if (linearTabs.getChildAt(i) is TextView){
+                var tv=linearTabs.getChildAt(i) as TextView
+                tv.setBackgroundResource(R.color.transparent)
+                AppHelper.setTextColor(requireContext(),tv,R.color.redPrimary)
+            }
+        }
+        tvSelected.setBackgroundResource(R.drawable.rounded_red_background)
+        AppHelper.setTextColor(requireContext(),tvSelected,R.color.white)
+
+    }
+
+    private fun setData(type:Boolean){
+        var adapter = AdapterOrderType(ordersArray, this, requireContext(),type)
+        rvOrderDetails.adapter = adapter
+        var glm2 = GridLayoutManager(requireContext(), 1)
+        rvOrderDetails.layoutManager = glm2
     }
 }
